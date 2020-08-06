c99124641de05810a87df21631de68624cfced1a
Kalimar Maia <kmaia@stsiinc.com>
2020-08-06 14:39:08 -0400

    Add date input component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FancyButton, DateInput, DateInputGroup, Fieldset } from 'kalimarcomponents/dist';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ width: 640, margin: "15px auto" }}>
+     <Fieldset legend="Date of birth">
+  <span className="usa-hint" id="dateOfBirthHint">
+    For example: 4 28 1986
+  </span>
+  <DateInputGroup>
+    <DateInput
+      id="testDateInput"
+      name="testName"
+      label="Month"
+      unit="month"
+      maxLength={2}
+      minLength={2}
+    />
+    <DateInput
+      id="testDateInput"
+      name="testName"
+      label="Day"
+      unit="day"
+      maxLength={2}
+      minLength={2}
+    />
+    <DateInput
+      id="testDateInput"
+      name="testName"
+      label="Year"
+      unit="year"
+      maxLength={4}
+      minLength={4}
+    />
+  </DateInputGroup>
+  <FancyButton ariaLabel="submit" type="button">Save the Date</FancyButton>
+  </Fieldset>
+    
     </div>
   );
 }
 
-export default App;
+export default App
