07ec249429da05152af34e5f09dfac74e5253ee4
jcklpe <david@jackalope.tech>
2020-08-07 04:05:28 -0500

    added scss support neatened up some styling. Couldn't get DateOfBirth component to work though

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
 import React from 'react';
 import './App.css';
 import { FancyButton, DateInput, DateInputGroup, Fieldset } from 'kalimarcomponents/dist';
+import "./app.scss";
 
 
 function App() {
   return (
+
     <div className="App" style={{ width: 640, margin: "15px auto" }}>
+
+
      <Fieldset legend="Date of birth">
-  <span className="usa-hint" id="dateOfBirthHint">
-    For example: 4 28 1986
-  </span>
-  <DateInputGroup>
-    <DateInput
+      <span className="usa-hint" id="dateOfBirthHint">
+      mm/dd/yyyy
+      </span>
+    <DateInputGroup>
+     <DateInput
       id="testDateInput"
       name="testName"
       label="Month"
       unit="month"
       maxLength={2}
       minLength={2}
-    />
-    <DateInput
-      id="testDateInput"
-      name="testName"
-      label="Day"
-      unit="day"
-      maxLength={2}
+      />
+      <DateInput
+        id="testDateInput"
+        name="testName"
+       label="Day"
+       unit="day"
+        maxLength={2}
       minLength={2}
     />
     <DateInput
@@ -38,7 +42,7 @@ function App() {
   </DateInputGroup>
   <FancyButton ariaLabel="submit" type="button">Save the Date</FancyButton>
   </Fieldset>
-    
+
     </div>
   );
 }
