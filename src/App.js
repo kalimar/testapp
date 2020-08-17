65daf1132fa763ba5fec5ed69d602601437822d1
Kalimar Maia <kmaia@stsiinc.com>
2020-08-17 07:48:45 -0400

    Table example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import {
+  Table,
   FancyButton,
   DateInput,
   DateInputGroup,
@@ -34,7 +35,7 @@ function App() {
             minLength={2}
           />
           <DateInput
-            id="testDateInputYear"
+            id="testDateInputYear N"
             name="testName"
             label="Year"
             unit="year"
@@ -46,6 +47,46 @@ function App() {
           Save the Date
         </FancyButton>
       </Fieldset>
+      <Table
+        bordered={true}
+        fullWidth={true}
+        caption="This table has very important numbers"
+      >
+        <thead>
+          <tr>
+            <th id="numbers">Important Numbers</th>
+            <th id="significance">Significance to answers</th>
+            <th id="popularity">Popularity</th>
+            <th id="decimals">Decimal Places</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th id="hitchhikers" headers="numbers">
+              42
+            </th>
+            <th headers="significance hitchhikers">100%</th>
+            <th headers="popularity hitchhikers">50%</th>
+            <th headers="decimals hitchhikers">none</th>
+          </tr>
+          <tr>
+            <th id="pi" headers="numbers">
+              pi
+            </th>
+            <th headers="pi significance">80%</th>
+            <th headers="pi popularity">100%</th>
+            <th headers="pi decimals">infinite</th>
+          </tr>
+          <tr>
+            <th id="tau" headers="numbers">
+              tau
+            </th>
+            <th headers="tau significance">90%</th>
+            <th headers="tau popularity">20%</th>
+            <th headers="tau decimals">infinite</th>
+          </tr>
+        </tbody>
+      </Table>
       <Alert
         type="warning"
         heading="warning status"
